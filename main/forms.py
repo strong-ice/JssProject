bdef7b44c23d4d93a04bc8900cd7632ca6ad4a7b
strong-ice <binghwa0610@gmail.com>
2020-08-16 23:38:37 +0900

    CssError2

--- main/forms.py
+++ main/forms.py
@@ -6,12 +6,12 @@ class JssForm(forms.ModelForm):
     class Meta:
         model = Jasoseol
         fields = ('title', 'content',)
-        
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['title'].label = "제목"
-            self.fields['content'].label = "자기소개서 내용"
-            self.fields['title'].widget.attrs.update({
-                'class': 'jss_title',
-                'placeholder': '제목',
-            })
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].label = "제목"
+        self.fields['content'].label = "자기소개서 내용"
+        self.fields['title'].widget.attrs.update({
+            'class': 'jss_title',
+            'placeholder': '제목',
+        })
