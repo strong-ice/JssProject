fd34ebfaad92ab1d8a89bdf4559f230046c3be03
strong-ice <binghwa0610@gmail.com>
2020-08-17 20:45:09 +0900

    pkError

--- JssProject/urls.py
+++ JssProject/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import index, create
+from main.views import index, create, detail, delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
     path('create/', create, name="create"),
+    path('detail/<int:jss_id>', detail, name="detail"),
+    path('delete/<int:jss_id>', delete, name="delete"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import JssForm
 from .models import Jasoseol
+from django.http import Http404
 
 
 def index(request):
@@ -14,4 +15,30 @@ def create(request):
             filled_form.save()
             return redirect('index')
     jss_form = JssForm()
+    return render(request, 'create.html', {'jss_form':jss_form})
+
+def detail(request, jss_id):
+    # try:
+    #     my_jss = Jasoseol.objects.get(pk=jss_id)
+    # except:
+    #     raise Http404
+
+    my_jss = get_object_or_404(Jasoseol, pk=jss_id)
+
+    return render(request, 'detail.html', {'my_jss':my_jss})
+
+def delete(request, jss_id):
+    my_jss = Jasoseol.objects.get(pk=jss_id)
+    my_jss.delete()
+    return redirect('index')
+
+def update(request, jss_id):
+    my_jss = Jasoseol.objects.get(pk=jss_id)
+    jss_form = JssForm(instance=my_jss)
+    if request.method =="POST":
+        updated_form = JssForm(request.POST, instance=my_jss)
+        if updated_form.is_vaild():
+            updated_form.save()
+            return redirect('index')
+
     return render(request, 'create.html', {'jss_form':jss_form})
