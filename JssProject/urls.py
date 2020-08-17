22f7a459f768150dd3c3f07ce5bcb02ff5157817
strong-ice <binghwa0610@gmail.com>
2020-08-17 22:17:07 +0900

    user1End

--- JssProject/urls.py
+++ JssProject/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main.views import index, create, detail, delete, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
-    path('create/', create, name="create"),
-    path('detail/<int:jss_id>', detail, name="detail"),
-    path('delete/<int:jss_id>', delete, name="delete"),
-    path('update/<int:jss_id>', update, name="update"),
+    path('', include('main.urls')),
+    path('', include('accounts.urls')),
 ]
